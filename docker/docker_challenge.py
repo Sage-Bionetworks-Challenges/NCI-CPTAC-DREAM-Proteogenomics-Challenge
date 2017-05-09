ca196ebe1404d3ff666a8f53863884f5721c7241
Thomas Yu <thomasyu888@gmail.com>
2017-05-09 15:19:18 -0700

    Status might not have annotations

--- docker/docker_challenge.py
+++ docker/docker_challenge.py
@@ -99,9 +99,12 @@ def update_single_submission_status(status, add_annotations, force=False):
                If dict, all submissions will be added as private submissions
     """
     existingAnnotations = status.get("annotations")
-
-    privateAnnotations = {each['key']:each['value'] for annots in existingAnnotations for each in existingAnnotations[annots] if annots not in ['scopeId','objectId'] and each['isPrivate'] == True}
-    publicAnnotations = {each['key']:each['value'] for annots in existingAnnotations for each in existingAnnotations[annots] if annots not in ['scopeId','objectId'] and each['isPrivate'] == False}
+    if existingAnnotations is not None:
+        privateAnnotations = {each['key']:each['value'] for annots in existingAnnotations for each in existingAnnotations[annots] if annots not in ['scopeId','objectId'] and each['isPrivate'] == True}
+        publicAnnotations = {each['key']:each['value'] for annots in existingAnnotations for each in existingAnnotations[annots] if annots not in ['scopeId','objectId'] and each['isPrivate'] == False}
+    else:
+        privateAnnotations = {}
+        publicAnnotations = {}
 
     if not synapseclient.annotations.is_submission_status_annotations(add_annotations):
         privateAddedAnnotations = add_annotations
