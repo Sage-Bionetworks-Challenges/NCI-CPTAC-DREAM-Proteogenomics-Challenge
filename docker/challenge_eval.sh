0cad7bcf82f24271cca88fa86cebcc249d74e12b
thomasyu888 <thomasyu888@gmail.com>
2017-06-09 15:05:49 -0700

    Use environment variables

--- docker/challenge_eval.sh
+++ docker/challenge_eval.sh
@@ -1,12 +1,17 @@
 # Automation of validation and scoring
 # Make sure you point to the directory where challenge.py belongs and a log directory must exist for the output
-cd ./
+cd ~/NCI-CPTAC-Challenge/docker
 #---------------------
 #Validate submissions
 #---------------------
-python docker_challenge.py -u "synapse user here" -p "password" --send-messages --acknowledge-receipt --notifications validate --all >> log/score.log 2>&1
+python docker_challenge.py --acknowledge-receipt --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications validate --all >> ~/log/score.log 2>&1
 
 #--------------------
 #Score submissions
 #--------------------
-python docker_challenge.py -u "synpase user here" -p "password" --send-messages --notifications score --all >> log/score.log 2>&1
+python docker_challenge.py --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications score --all >> ~/log/score.log 2>&1
+
+#--------------------
+#Stop submissions
+#--------------------
+#python docker_challenge.py -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications dockerstop --all >> ~/log/score.log 2>&1
