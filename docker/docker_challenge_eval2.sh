e46bb8e3a66262d8aa2005493f83bf9c6078b51b
Thomas Yu <thomasyu888@gmail.com>
2017-06-19 11:38:15 -0700

    Add in shell scripts for each evaluation queue

---
+++ docker/docker_challenge_eval2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Automation of validation and scoring
+# Make sure you point to the directory where challenge.py belongs and a log directory must exist for the output
+# Set environment variables for synapse username and password
+cd ~/NCI-CPTAC-Challenge/docker
+#---------------------
+#Validate submissions
+#---------------------
+#Proteogenomics Subchallenge 2 (8720145)
+python docker_challenge.py --acknowledge-receipt --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications validate 8720145 >> ~/log/score.log 2>&1
+
+#--------------------
+#Score submissions
+#--------------------
+python docker_challenge.py --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications score 8720145 >> ~/log/score.log 2>&1
+
+#--------------------
+#Stop submissions
+#--------------------
+#python docker_challenge.py -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications dockerstop 8720145 >> ~/log/score.log 2>&1
---
+++ docker/docker_expresslanes3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Automation of validation and scoring
+# Make sure you point to the directory where challenge.py belongs and a log directory must exist for the output
+# Set environment variables for synapse username and password
+cd ~/NCI-CPTAC-Challenge/docker
+#---------------------
+#Validate submissions
+#---------------------
+#Proteogenomics Subchallenge 3 Express (9604718)
+python docker_challenge.py --acknowledge-receipt --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications validate 9604718 >> ~/log/score.log 2>&1
+
+#--------------------
+#Score submissions
+#--------------------
+python docker_challenge.py --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications score 9604718 >> ~/log/score.log 2>&1
+
+#--------------------
+#Stop submissions
+#--------------------
+#python docker_challenge.py -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications dockerstop 9604718 >> ~/log/score.log 2>&1
