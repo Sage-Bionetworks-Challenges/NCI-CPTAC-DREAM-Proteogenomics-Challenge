6ae6067b9b13dcb16d9f23dc205d036aa7caa4a3
thomasyu888 <thomasyu888@gmail.com>
2017-07-16 01:43:33 -0700

    Update NMRSE function

--- scoring_harness/scoring_functions.R
+++ scoring_harness/scoring_functions.R
@@ -67,19 +67,19 @@ correlation_by_row <- function(pred_path, truth_path) {
 #result_corr <- correlation_by_row("predictions.tsv", "pros_ova_proteome_sort_common_gene_6577.txt")
 
 ########################################## load NRMSE function #########################################
-NRMSE_by_row <- function(pred_path, truth_path) {
+NRMSE_by_row <- function(pred_path, truth_path)  {
   suppressPackageStartupMessages(library(hydroGOF))
   prediction <- read.csv( pred_path, row.names = 1 , check.names = F, sep="\t") 
   test_prot  <- read.csv( truth_path, row.names = 1 , check.names = F, sep="\t")
   common_protein <- intersect(rownames(prediction), rownames(test_prot))
-  prediction <- prediction[common_protein, colnames(test_prot)]
-  test_prot <- test_prot[common_protein, colnames(test_prot)]
+  prediction <- prediction[common_protein , colnames(test_prot) ]
+  test_prot <- test_prot[common_protein , colnames(test_prot)]
   mat1 <- as.matrix(prediction)
   mat2 <- as.matrix(test_prot) 
   nrmse_vec <- c()
   for(i in 1:length(mat1[ ,1]) ) {
-    temp <- hydroGOF::rmse(mat1[ i, ], mat2[ i , ])
-    nrmse_vec <- c(nrmse_vec , temp/(max(mat2,na.rm = T)-min(mat2,na.rm = T)))
+    temp <- hydroGOF::rmse(mat1[i,], mat2[i,],na.rm=T)
+    nrmse_vec <- c(nrmse_vec , temp/(max(mat2[i,],na.rm=T)-min(mat2[i,],na.rm=T)))
   }
   names(nrmse_vec) <- rownames(mat1)
   return(mean(nrmse_vec))
