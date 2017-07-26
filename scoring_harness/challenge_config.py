77e99bbf066e2676feaac738564fc4d151c91e87
Thomas Yu <thomasyu888@gmail.com>
2017-07-26 11:11:43 +0200

    Variance is 0 across protein

--- scoring_harness/challenge_config.py
+++ scoring_harness/challenge_config.py
@@ -59,7 +59,7 @@ def _validate_func_helper(filePath, goldDf, predOrConf, column="proteinID", vari
     assert all(goldDf.columns.isin(fileDf.columns)), "%s: All sample Ids in the goldstandard must also be in your file. You are missing: %s" % (fileName, ",".join(goldDf.columns[~goldDf.columns.isin(fileDf.columns)]))
     assert all(~fileDf.isnull()), "%s: There can't be any null values" % fileName
     if varianceCheck:
-        assert all([pd.np.var(fileDf[i]) != 0 for i in fileDf]), "%s: No columns can have variance of 0" % fileName
+        assert all(fileDf.apply(pd.np.var,axis=0) != 0), "%s: No columns can have variance of 0" % fileName
 
 
 def validate_func1(dirName, goldstandard_path, column):
