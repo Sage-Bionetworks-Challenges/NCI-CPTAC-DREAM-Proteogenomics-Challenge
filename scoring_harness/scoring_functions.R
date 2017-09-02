6d2f159f82d5e9df1b4a0298838b0fe300d69ea8
Katan5555 <Katan5555@users.noreply.github.com>
2017-09-02 18:00:08 +0200

    Update scoring_functions.R

--- scoring_harness/scoring_functions.R
+++ scoring_harness/scoring_functions.R
@@ -32,6 +32,11 @@ correlation_by_row <- function(pred_path, truth_path) {
 
   mat1 <- as.matrix(prediction)
   mat2 <- as.matrix(test_prot) 
+  
+  # consider only proteins with less than 30% missing values
+  mat2 <- mat2 [ which(rowMeans(is.na(mat2)) < 0.3) , ]
+  mat1 <- mat1 [rownames(mat2), ]
+  
   corr_vec <- c()
   for(i in 1:length(mat1[ ,1]) ) {
     temp <- cor.test(mat1[ i, ], mat2[ i , ])
