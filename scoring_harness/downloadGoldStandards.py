5ce1978a2398942b63e37f86c49bedc1f2331438
Thomas Yu <thomasyu888@gmail.com>
2017-09-06 11:39:28 -0700

    Download new goldstandards

--- scoring_harness/downloadGoldStandards.py
+++ scoring_harness/downloadGoldStandards.py
@@ -11,6 +11,7 @@ def downloadData(syn, parentId, testDataDir):
 
 def main():
 	parser = argparse.ArgumentParser()
+	parser.add_argument("round", help="Round", choices=['1','2',"final"])
 	parser.add_argument("--express", help="Express lane", action="store_true", default=False)
 	args = parser.parse_args()
 	syn = synapseclient.login()
@@ -18,11 +19,23 @@ def main():
 	if not os.path.exists(downloadDir):
 		os.mkdir(downloadDir)
 	
-	downloadData(syn, "syn10165897",downloadDir)
-	downloadData(syn, "syn10165898",downloadDir)
-	downloadData(syn, "syn10165900",downloadDir)
+	# downloadData(syn, "syn10165897",downloadDir)
+	# downloadData(syn, "syn10165898",downloadDir)
+	# downloadData(syn, "syn10165900",downloadDir)
 	downloadData(syn, "syn10164401",downloadDir)
-
+	sc1 = syn.get("syn10164502")
+	if args.round == '1':
+		sc2 = syn.get("syn10617817")
+		sc3 = syn.get("syn10617831")
+	elif args.round == '2':
+		sc2 = syn.get("syn10617818")
+		sc3 = syn.get("syn10617838")
+	else:
+		sc2 = syn.get("syn10617819")
+		sc3 = syn.get("syn10617830")
+	shutil.copy(sc1.path, downloadDir)
+	shutil.copy(sc2.path, os.path.join(downloadDir, "prospective_ova_pro_gold.txt"))
+	shutil.copy(sc3.path, os.path.join(downloadDir, "prospective_ova_phospho_gold.txt"))
 	# if args.express:
 	# 	shutil.copy(cna.path, testDataDir)
 	# 	shutil.copy(proteome.path, "%s/pros_ova_proteome_sort_common_gene_6577.txt" % testDataDir)
