6593fcb6cab3008c599a1914eb6ce0012a55d644
Thomas Yu <thomasyu888@gmail.com>
2017-09-14 13:36:33 -0700

    docker chal internal

---
+++ docker/docker_challenge_internal1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Automation of validation and scoring
+# Make sure you point to the directory where challenge.py belongs and a log directory must exist for the output
+# Set environment variables for synapse username and password
+script_dir=$(dirname $0)
+if [ ! -d "$script_dir/log" ]; then
+  mkdir $script_dir/log
+fi
+if [ ! -d "$script_dir/output" ]; then
+  mkdir $script_dir/output
+fi
+#---------------------
+#Validate submissions
+#---------------------
+#Proteogenomics Subchallenge 1 (8720143)
+python $script_dir/docker_challenge.py --acknowledge-receipt --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications validate 9606530 >> $script_dir/log/score.log 2>&1
+
+#--------------------
+#Score submissions
+#--------------------
+python $script_dir/docker_challenge.py --canCancel -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications score 9606530 >> $script_dir/log/score.log 2>&1
+
+#--------------------
+#Stop submissions
+#--------------------
+#python $script_dir/docker_challenge.py -u $SYNAPSE_USER -p $SYNAPSE_PASS --send-messages --notifications dockerstop 9606530 >> $script_dir/log/score.log 2>&1
