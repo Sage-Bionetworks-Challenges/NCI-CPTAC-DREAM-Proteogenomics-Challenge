38b0439ba0cd2fc9c951c96b3d7b4ff1cefc4122
thomasyu888 <thomasyu888@gmail.com>
2017-09-28 10:05:01 -0700

    Make sure to pass in the numerical value

--- docker/downloadTestData.py
+++ docker/downloadTestData.py
@@ -56,7 +56,7 @@ def main():
 		removeFile = os.path.join(testDataDir, "data_true.txt")
 		if args.express:
 			for i in range(1,101):
-				os.rename(os.path.join(testDataDir, "data_obs_%s.txt"), os.path.join(testDataDir, "data_test_obs_%s.txt"))
+				os.rename(os.path.join(testDataDir, "data_obs_%s.txt" % i), os.path.join(testDataDir, "data_test_obs_%s.txt" % i))
 	elif args.sc == 'sc2':
 		downloadData(syn, sc2, testDataDir)
 		removeFile = os.path.join(testDataDir, "prospective_ova_pro_gold_express.txt")
