331c3347b4de16685d957afd4d404496145d79bf
Thomas Yu <thomasyu888@gmail.com>
2017-10-12 16:22:58 -0700

    Rename test data files

--- docker/downloadTestData.py
+++ docker/downloadTestData.py
@@ -57,6 +57,9 @@ def main():
 		if args.express:
 			for i in range(1,101):
 				os.rename(os.path.join(testDataDir, "data_obs_%s.txt" % i), os.path.join(testDataDir, "data_test_obs_%s.txt" % i))
+		elif args.round == "final":
+			for i in range(1,101):
+				os.rename(os.path.join(testDataDir, "data_test_obs_%s.txt" % i+100), os.path.join(testDataDir, "data_test_obs_%s.txt" % i))
 	elif args.sc == 'sc2':
 		downloadData(syn, sc2, testDataDir)
 		removeFile = os.path.join(testDataDir, "prospective_ova_pro_gold_express.txt")
