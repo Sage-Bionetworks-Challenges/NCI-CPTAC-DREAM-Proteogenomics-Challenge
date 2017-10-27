0514fcab568c20e2f040d5b3d47dd5ce2da47b81
thomasyu888 <thomasyu888@gmail.com>
2017-10-27 10:52:08 -0700

    Update messages

--- docker/messages.py
+++ docker/messages.py
@@ -16,8 +16,8 @@ dry_run = False
 
 ## Edit these URLs to point to your challenge and its support forum
 defaults = dict(
-    challenge_instructions_url = "https://www.synapse.org/",
-    support_forum_url = "http://support.sagebase.org/sagebase",
+    challenge_instructions_url = "https://www.synapse.org/#!Synapse:syn8228304/wiki/448381",
+    support_forum_url = "https://www.synapse.org/#!Synapse:syn8228304/discussion/default",
     scoring_script = "the scoring script")
 
 ##---------------------------------------------------------
