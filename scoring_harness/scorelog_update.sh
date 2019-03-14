e21076caad2e654893e05af0e43656417c207d58
MI YANG <miyang@Phantom.local>
2019-03-14 09:51:29 +0100

    Remove duplicated directory

--- scoring_harness/scorelog_update.sh
+++
@@ -1,3 +0,0 @@
-# Make sure you create a directory where you want to keep your log files in this case (~/log)
-script_dir=$(dirname $0)
-cd $script_dir/log && mv score.log score`date +"%Y_%m_%d"`.log && touch score.log
